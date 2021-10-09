aba0bc0afa21eebb80660df3221870c7c7e80327
NaumanWasti <87071367+NaumanWasti@users.noreply.github.com>
2021-10-09 16:40:56 +0500

    Add files via upload

---
+++ class.py
@@ -0,0 +1,11 @@
+class employee:
+    def __init__(self,name,age,role):
+        self.name=name
+        self.age=age
+        self.role=role
+  
+    
+employee1=employee("nauman",38,"manager")
+employee2=employee("ali",48,"peon")
+
+print(employee1.name)
